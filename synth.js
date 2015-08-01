1df166e9481e3724d5d896b90a38565a7444f78f
Stuart Memo <stuartmemo@gmail.com>
2015-08-01 17:36:04 +0100

    Oscillator for every keyDown

--- synth.js
+++ synth.js
@@ -1,6 +1,7 @@
 var context = new AudioContext(),
-    osc = context.createOscillator(),
-    volume = context.createGain();
+    volume = context.createGain(),
+    sawtoothOscillators = {},
+    squareOscillators = {};
 
 var keyboard = new QwertyHancock({
     id: 'keyboard',
@@ -9,15 +10,31 @@ var keyboard = new QwertyHancock({
 
 volume.gain.value = 0.5;
 
-osc.connect(volume);
 volume.connect(context.destination);
 
-
 keyboard.keyDown = function (note, frequency) {
+    var osc = context.createOscillator(),
+        squareOsc = context.createOscillator();
+
+    sawtoothOscillators[note] = osc;
+    squareOscillators[note] = squareOsc;
+
+    osc.connect(volume);
+    squareOsc.connect(volume);
+
+    osc.type = 'sawtooth';
+    squareOsc.type = 'square';
     osc.frequency.value = frequency;
+    osc.detune.value = 10;
+    squareOsc.frequency.value = frequency;
+    squareOsc.detune.value = -10;
     osc.start(context.currentTime);
+    squareOsc.start(context.currentTime);
 };
 
-keyboard.keyUp = function () {
-    osc.stop(context.currentTime);
+keyboard.keyUp = function (note, frequency) {
+    sawtoothOscillators[note].stop(context.currentTime);
+    sawtoothOscillators[note].disconnect();
+    squareOscillators[note].stop(context.currentTime);
+    squareOscillators[note].disconnect();
 };
